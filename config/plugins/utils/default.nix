261962f2f8422b259cb76d8e3f0930749b98ba32
IldenH <IldenH.1@proton.me>
2024-05-10 17:45:14 +0200

    Utils: more plugins

--- config/plugins/utils/default.nix
+++ config/plugins/utils/default.nix
@@ -1,9 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
 	imports = [
 		./hop.nix
 		./sessions.nix
 		./which-key.nix
+	];
+	
+	plugins = {
+		comment.enable = true;
+		nvim-autopairs.enable = true;
+	};
+
+	extraPlugins = with pkgs.vimPlugins; [
+		mkdir-nvim
 	];
 }

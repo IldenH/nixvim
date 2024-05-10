2eed0fff9b05743d28a8805f24094597b0d93be1
IldenH <IldenH.1@proton.me>
2024-05-10 16:28:53 +0200

    Options: cleaned

--- config/options.nix
+++ config/options.nix
@@ -1,62 +1,60 @@
 {
-	config = {
-		opts = {
-			encoding = "UTF-8";
-			fileencoding = "utf-8";
-			mousemoveevent = true;
-			hidden = true;
-			ch = 0;
-			pumheight = 10;
-			winminwidth = 10;
-			splitbelow = true;
-			splitright = true;
-			list = true;
-			termguicolors = true;
-			showtabline = 3;
-			tabline = " ";
-			showmode = false;
-			backup = false;
-			number = true;
-			relativenumber = true;
-			numberwidth = 3;
-			ruler = false;
-			writebackup = false;
-			updatetime = 250;
-			timeoutlen = 100;
-			hlsearch = true;
-			incsearch = true;
-			ignorecase = true;
-			smartcase = true;
-			scrolloff = 0;
-			sidescrolloff = 5;
-			foldcolumn = "0";
-			foldlevel = 99;
-			foldlevelstart = 99;
-			foldenable = true;
-			mouse = "a";
-			cursorline = true;
-			tabstop = 2;
-			softtabstop = 2;
-			shiftwidth = 2;
-			autoindent = true;
-			smartindent = true;
-			signcolumn = "yes";
-			laststatus = 0;
-			shadafile = "NONE";
-			swapfile = false;
-			undofile = true;
-			undolevels = 10000;
-		};
-
-		globals = {
-			mapleader = " ";
-			tex_flavor = "latex";
-			code_action_menu_window_border = "single";
-		};
-
-    clipboard = {
-      register = "unnamedplus";
-      providers.wl-copy.enable = true;
-    };
+	opts = {
+		encoding = "UTF-8";
+		fileencoding = "utf-8";
+		mousemoveevent = true;
+		hidden = true;
+		ch = 0;
+		pumheight = 10;
+		winminwidth = 10;
+		splitbelow = true;
+		splitright = true;
+		list = true;
+		termguicolors = true;
+		showtabline = 3;
+		tabline = " ";
+		showmode = false;
+		backup = false;
+		number = true;
+		relativenumber = true;
+		numberwidth = 3;
+		ruler = false;
+		writebackup = false;
+		updatetime = 250;
+		timeoutlen = 100;
+		hlsearch = true;
+		incsearch = true;
+		ignorecase = true;
+		smartcase = true;
+		scrolloff = 0;
+		sidescrolloff = 5;
+		foldcolumn = "0";
+		foldlevel = 99;
+		foldlevelstart = 99;
+		foldenable = true;
+		mouse = "a";
+		cursorline = true;
+		tabstop = 2;
+		softtabstop = 2;
+		shiftwidth = 2;
+		autoindent = true;
+		smartindent = true;
+		signcolumn = "yes";
+		laststatus = 0;
+		shadafile = "NONE";
+		swapfile = false;
+		undofile = true;
+		undolevels = 10000;
 	};
+
+	globals = {
+		mapleader = " ";
+		tex_flavor = "latex";
+		code_action_menu_window_border = "single";
+	};
+
+  clipboard = {
+    register = "unnamedplus";
+    providers.wl-copy.enable = true;
+  };
 }
